5e1ac4b7df9dd08a2484903f9fa2458639591f1d
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-03-19 23:03:30 +0100

    feat: Initial form for adding highscores

---
+++ src/Form.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useRef, FormEvent } from "react"
+import { FormLabel, FormControl, Input, Button } from "@chakra-ui/react"
+import { P } from "@northlight/ui"
+
+type FormData = {
+  name: string
+  score: number
+}
+
+type FormProps = {
+  onFormSubmit: (data: FormData) => void
+}
+
+export const Form = ({ onFormSubmit }: FormProps) => {
+  const [name, setName] = useState("")
+  const [score, setScore] = useState<number | null>()
+  const [error, setError] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const updateField = (e: FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget
+    setError(false)
+    if (name === "name") setName(value)
+    if (name === "score") setScore(Number(value))
+  }
+
+  const validateAndSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (!name || !score) {
+      setError(true)
+      return
+    }
+
+    onFormSubmit({ name, score })
+    setName("")
+    setScore(null)
+
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }
+
+  return (
+    <form onSubmit={validateAndSubmit}>
+      <FormControl isInvalid={error}>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onInput={updateField}
+          ref={inputRef}
+        />
+      </FormControl>
+
+      <FormControl isInvalid={error}>
+        <FormLabel htmlFor="score">Score</FormLabel>
+        <Input
+          type="number"
+          name="score"
+          value={score ? score.toString() : ""}
+          onInput={updateField}
+        />
+      </FormControl>
+
+      {error && <P>Both fields are required</P>}
+
+      <Button mt={4} colorScheme="teal" type="submit">
+        Submit
+      </Button>
+    </form>
+  )
+}
--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,10 @@ import { ScoreBoard } from "./ScoreBoard"
 import { mapScoresToUsers, getHighscores } from "./helpers.js"
 import scores from "./scores"
 import users from "./users"
+import { Form } from "./Form"
+import { ResultObject } from "./types"
+
+type FormData = { name: string; score: number }
 
 const initialUserScores = mapScoresToUsers(scores, users)
 
@@ -21,6 +25,24 @@ export default function App() {
     setHighscores(newHighscores)
   }
 
+  const handleFormSubmit = (formData: FormData) => {
+    const newHighscores: ResultObject = {
+      ...highscores,
+      [formData.name]: highscores[formData.name]
+        ? Math.max(highscores[formData.name], formData.score)
+        : formData.score,
+    }
+
+    const updatedHighscores = Object.entries(newHighscores).map(
+      ([name, score]) => ({
+        name,
+        score,
+      })
+    )
+
+    setHighscores(getHighscores(updatedHighscores))
+  }
+
   return (
     <Container maxW="6xl" padding="4">
       <H1 marginBottom="4">Mediatool exercise</H1>
@@ -30,6 +52,10 @@ export default function App() {
           label="Import excel file here"
         />
         <VStack align="left">
+          <Box>
+            <Form onFormSubmit={handleFormSubmit} />
+          </Box>
+
           <Box>
             <ScoreBoard highscores={highscores} />
           </Box>
