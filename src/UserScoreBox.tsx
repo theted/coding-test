9cf95e2aa0f0ada0bc6902005636e342e365a43c
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-03-31 22:19:29 +0200

    fix: Avoid key collisions for multiple occurencies of the same score

--- src/UserScoreBox.tsx
+++ src/UserScoreBox.tsx
@@ -12,8 +12,8 @@ export const UserScoreBox = ({ name, allScores }: UserScoreBoxProps) => (
   <Container p="0" mb="4">
     <Fade in>
       <H4>{name}</H4>
-      {filterUserScores(name, allScores).map((row) => (
-        <Box key={row.score}>{row.score}</Box>
+      {filterUserScores(name, allScores).map(({ score }, index) => (
+        <Box key={`${score}-${index}`}>{score}</Box>
       ))}
     </Fade>
   </Container>
